6b84d5ff6d449809daf1b3deb9c05274311cf124
andre kano <andrekano@outlook.com>
2024-02-23 14:38:51 -0300

    Challenge Rails Task Manager

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_162720) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_184438) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "details"
+    t.text "details"
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
